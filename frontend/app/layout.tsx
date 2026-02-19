9ff095d0e4acb74e42e23027f732064561446361
Terence Dinguilu <terence.dinguilu@gmail.com>
2026-02-19 16:16:08 +0100

    transactions

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Toaster/>
+        <div className="flex justify-center items-center min-h-screen my-5">
+          {children}
+        </div>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,7 @@
 "use client"
+import { useEffect, useState } from "react";
+import api from "./api";
+import toast from "react-hot-toast";
 type Transaction = {
   id : string ;
   text:string;
@@ -7,6 +10,34 @@ type Transaction = {
 }
 
 export default function Home() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  const getTransactions = async()=> {
+    try {
+      const res = await api.get<Transaction[]>("transactions/")
+      setTransactions(res.data)
+      toast.success("Transactions chargées")
+    } catch (error) {
+      console.error("Erreur chargement transactions" , error);
+      toast.error("Erreur chargement transactions");
+      
+    }
+  }
+
+  useEffect(() => {
+    getTransactions()
+  }, []);
+
+  const amounts = transactions.map((t) => Number(t.amount)   || 0 )
+  const balance = amounts.reduce((acc , item) => acc + item , 0)
+  const income = 
+  amounts.filter((a) => a > 0).reduce((acc , item) => acc + item , 0)
+
+  const expense = 
+  amounts.filter((a) => a < 0).reduce((acc , item) => acc + item , 0)
+
+
+
   return (
     <button className="btn btn-sm">
       terence
